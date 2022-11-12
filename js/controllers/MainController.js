647c967908288c591d6340c131ca3ab0cb8c6d8a
Verne Hartikainen <verne.hartikainen@gmail.com>
2022-11-12 16:25:59 +0200

    working

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,14 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.list = ["test1", "test2"]
+    $scope.todo = {
+        title: "things to do",
+        list: ["test1", "test2"]    
+    }   
+    $scope.books = {
+        title: "books to get",
+        list: []
+    }
+    $scope.addItem = function(itemList, item) {
+        console.log("moro")
+        itemList.push(item);
+    }
 }])
